4ba385d1f63cb8f5d98ca03e2f745db8ea579956
TheBarbaraIsTaken <szabo.barbara.noemi@gmail.com>
2024-04-26 20:46:13 +0200

    failure simulations

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from qubit import Qubit
 from bb84 import BB84
+import numpy as np
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     # Test
@@ -14,9 +16,52 @@ if __name__ == "__main__":
     # q.hadamard()
     # print(q)
     
-    # BB84 protocol
-    protocol = BB84(length=10)
+    # protocol = BB84(length=100_000)
 
-    for i in range(10):
-        print(protocol.run(is_attack=True))
+    # is_failed = protocol.run(is_attack=False)
+    # print(protocol.joint_distr())
+
+    # print(is_failed)
+
     
+    # BB84 protocol failure for no attack
+    protocol = BB84()
+    N = 10_000
+    failures = []
+
+    lengths = list(range(1, 25))
+
+    for length in lengths:
+        protocol.length = length
+
+        tmp = []
+        for i in range(N):
+            is_failed = protocol.run(is_attack=False)
+            protocol.clear()
+            tmp.append(is_failed)
+
+        failures.append(sum(tmp))
+
+    # BB84 protocol failure with attack
+    protocol = BB84()
+    N_ATTACK = 10_000
+    failures_attack = []
+
+    lengths_attack = list(range(1, 25, 1))
+
+    for length in lengths_attack:
+        protocol.length = length
+
+        tmp = []
+        for i in range(N_ATTACK):
+            is_failed = protocol.run(is_attack=True)
+            protocol.clear()
+            tmp.append(is_failed)
+
+        failures_attack.append(sum(tmp))
+
+    plt.scatter(lengths, np.array(failures) / N, c='g')
+    plt.show()
+
+    plt.scatter(lengths_attack, np.array(failures_attack) / N_ATTACK, c='g')
+    plt.show()
